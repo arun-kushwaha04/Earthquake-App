c55f5f803505283a57f685aa8a587f32ff165188
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-08-06 01:36:48 +0530

    Added login to select the date

--- app/src/main/java/com/example/earthquakeapp/MainActivity.kt
+++ app/src/main/java/com/example/earthquakeapp/MainActivity.kt
@@ -1,24 +1,82 @@
 package com.example.earthquakeapp
 
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.annotation.RequiresApi
+import com.example.earthquakeapp.databinding.ActivityMainBinding
 import com.example.earthquakeapp.model.model
 import com.example.earthquakeapp.services.earthquakeData
 import com.example.earthquakeapp.services.serviceBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var dateView: TextView
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        var startDateSelector = binding.startDateValue;
+        var endDateSelector = binding.endDateValue;
+        var datePickerLayout = binding.datePickerLayout;
+        var datePicker = binding.datePicker;
+        var closeDatePicker = binding.closeDatePicker;
+        var startDate = binding.startDateValue;
+        var endDate = binding.endDateValue;
+        val currentDateTime = LocalDateTime.now()
+
+        startDate.text = currentDateTime.format(DateTimeFormatter.ISO_DATE)
+        endDate.text = currentDateTime.format(DateTimeFormatter.ISO_DATE)
+
+
+
+        startDateSelector.setOnClickListener(){
+            datePickerLayout.visibility= View.VISIBLE
+//            var date:String="";
+//            date+=(datePicker.year);date+='-';
+//            date+=(datePicker.month+1);date+='-';
+//            date+=(datePicker.dayOfMonth);
+//            startDate.text=date;
+            dateView=startDate;
+        }
+        endDateSelector.setOnClickListener(){
+            datePickerLayout.visibility= View.VISIBLE;
+            dateView=endDate;
+        }
+
+
+        closeDatePicker.setOnClickListener(){
+            datePickerLayout.visibility= View.GONE
+        }
+
+        val today = Calendar.getInstance()
+        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH))
+        { _, year, months, day ->
+            val month = months + 1
+            if(currentDateTime.year >= year && currentDateTime.monthValue >= months && currentDateTime.dayOfMonth >= day)
+            dateView.text="${year}-${month}-${day}"
+            else dateView.text = currentDateTime.format(DateTimeFormatter.ISO_DATE)
+        }
+
+
+
 
         val earthquakeDataService = serviceBuilder.buildService(earthquakeData::class.java)
         val requestCall =
@@ -39,7 +97,7 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call<List<model>>, t: Throwable) {
                 Toast.makeText(
                     this@MainActivity,
-                    "Failed to Get Data Check Internet Connectivity",
+                    "Failed to Get Data Check Internet Connectivity2",
                     Toast.LENGTH_LONG
                 ).show()
             }
@@ -56,4 +114,5 @@ class MainActivity : AppCompatActivity() {
             spinner.adapter = adapter
         }
     }
+
 }
